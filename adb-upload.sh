486a6032834beecd383ff9c3ffb867644583d340
Robin <robin+git@rkslot.nl>
2022-01-19 10:09:36 +0100

    enable fan

--- adb-upload.sh
+++ adb-upload.sh
@@ -3,5 +3,6 @@ set -ex
 arm-linux-androideabi-clang++ -static-libstdc++ Governor.cpp -o Governor
 adb push Governor /data/local/workingdir/
 adb exec-out sh -c 'echo 0 > /sys/class/fan/mode'
+adb exec-out sh -c 'echo 1 > /sys/class/fan/enable'
 adb exec-out sh -c 'echo 4 > /sys/class/fan/level'
 adb exec-out sh -c 'cd /data/local/workingdir && chmod +x ./Governor && ./Governor graph_alexnet_all_pipe_sync 3 10 500'
