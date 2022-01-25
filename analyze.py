9dfa9854989e98cf108992960d0fbae86d2bf6c7
Robin <robin+git@rkslot.nl>
2022-01-25 16:31:31 +0100

    analysis

---
+++ analyze.py
@@ -0,0 +1,10 @@
+import csv
+import pandas
+
+data = pandas.read_csv('measurements.csv')
+print(data.groupby('partition point beide').mean())
+
+# with open('measurements.csv', 'r') as measurements_file:
+#     rows = csv.reader(measurements_file, delimiter=',')
+#     for row in rows:
+#         print(row)
