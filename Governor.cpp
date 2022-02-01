a2a0d893eedacc245fc9e83ec6a4e73c05542ed2
Robin <robin+git@rkslot.nl>
2022-02-01 18:12:34 +0100

    also implement big->gpu shift for G-B-L

--- Governor.cpp
+++ Governor.cpp
@@ -314,6 +314,12 @@ void tune_partition_points() {
 			} else {
 				more_tests_available = false;
 			}
+		} else if (order.compare("G-B-L")) {
+			if (partition_point_1 == 4) {
+				partition_point_1 = 3;
+			} else {
+				more_tests_available = false;
+			}
 		} else {
 			// TODO Implement shifting partition points for other pipeline orders
 			more_tests_available = false;
