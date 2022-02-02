43883f2a723f2505cb1750f0e16fe050c06483c6
Robin <robin+git@rkslot.nl>
2022-02-02 07:52:46 +0100

    script to set target

---
+++ modify-target.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ] || [ -z "$2" ]
+then
+    echo "Usage: $0 <target fps> <target latency>"
+    exit 1
+fi
+
+CONTENT="$1\n$2"
+
+adb exec-out sh -c "echo \"$CONTENT\" > target.txt"
